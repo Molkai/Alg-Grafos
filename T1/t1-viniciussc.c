d6f75eea02f8127b02e89ecc7f32f9622608923a
Vinícius <vc18051997@gmail.com>
2018-09-28 13:55:06 -0300

    Gerenciamento do grafo adicionado

--- T1/t1-viniciussc.c
+++ T1/t1-viniciussc.c
@@ -15,18 +15,25 @@ int main(){
 	scanf("%d %d", &n, &m);
 	while(n != 0 && m != 0){
 		graph = initGraph(n, m);
+
+
+		clean(graph, n);
 		scanf("%d %d", &n, &m);
 	}
 }
 
 edge **initGraph(int n, int m){
 	edge **new;
+	int x, y;
 
 	new = (edge **) malloc (n * sizeof(edge *));
 	while(m > 0){
-
+		scanf("%d %d", &x, &y);
+		addEdge(new[x-1], y);
+		addEdge(new[y-1], x);
 		m--;
 	}
+	return new;
 }
 
 edge *crEdge(int x){
@@ -36,4 +43,35 @@ edge *crEdge(int x){
 	new->vertex = x;
 	new->next = NULL;
 	return new;
+}
+
+void addEdge(edge **list, int x){
+	edge *new, *aux = *list;
+
+	new = crEdge(x);
+	if(*list == NULL || aux->vertex > x){
+		new->next = aux;
+		*list = new;
+	} else {
+		while(aux->next != NULL && aux->next->vertex < x){
+			aux = aux->next;
+		}
+		new->next = aux->next;
+		aux->next = new;
+	}
+}
+
+void clean (edge **list, int n){
+	edge *aux, *aux2;
+	int i;
+
+	for(i = 0; i < n; i++){
+		aux = list[i];
+		while(aux != NULL){
+			aux2 = aux->next;
+			free(aux);
+			aux = aux2;
+		}
+	}
+	free(list);
 }
